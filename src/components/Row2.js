74113f21aac7026962fcd234142aec9788bd5fde
Abdel <abdelwahaboufkir@pursuit.org>
2019-02-03 17:16:28 -0500

    layout of calculator is done

---
+++ src/components/Row2.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Row2 =(props)=>{
+    return(
+        <>
+             <button className="button col-3"> 7 </button>
+            <button className="button col-3"> 8 </button>
+            <button className="button col-3"> 9</button>
+            <button className="button col-3 orange">x</button>
+        </>
+    );
+}
+
+export default Row2;
